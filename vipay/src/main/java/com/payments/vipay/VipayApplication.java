73f1e9efd93a330c32649a7172006bb4bea0559e
avimehta14 <avimehta14@gmail.com>
2024-08-21 23:04:06 -0500

    setup

---
+++ vipay/src/main/java/com/payments/vipay/VipayApplication.java
@@ -0,0 +1,13 @@
+package com.payments.vipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipayApplication.class, args);
+	}
+
+}
---
+++ vipay/src/test/java/com/payments/vipay/VipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.vipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
