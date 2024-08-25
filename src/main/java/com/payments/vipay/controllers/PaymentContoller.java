5a2a329aa8550df5ee12d0526837bc0229be34a7
avimehta14 <avimehta14@gmail.com>
2024-08-25 14:06:16 -0500

    controller template

--- src/main/java/com/payments/vipay/controllers/PaymentContoller.java
+++ src/main/java/com/payments/vipay/controllers/PaymentContoller.java
@@ -1,12 +1,20 @@
 package com.payments.vipay.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pay")
 public class PaymentContoller {
 
+    @PostMapping
+    public ResponseEntity sendMoney() throws InterruptedException{
 
-
+        return new ResponseEntity<>(Map.of("message","Payment sent"), HttpStatus.OK);
+    }
 }
