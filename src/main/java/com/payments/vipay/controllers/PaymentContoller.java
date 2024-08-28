92f2211517ec346161fde4d9c8921d8a7c40d963
avimehta14 <avimehta14@gmail.com>
2024-08-28 00:09:32 -0500

    updated controller

--- src/main/java/com/payments/vipay/controllers/PaymentContoller.java
+++ src/main/java/com/payments/vipay/controllers/PaymentContoller.java
@@ -1,10 +1,12 @@
 package com.payments.vipay.controllers;
 
+import com.payments.vipay.data.PaymentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +17,10 @@ import java.util.Map;
 public class PaymentContoller {
 
     @Autowired
-    private final KafkaTemplate<String,PaymentRequest> kafkaTemplate;
-
+    private KafkaTemplate<String, PaymentRequest> kafkaTemplate;
 
     @PostMapping
-    public ResponseEntity sendMoney() throws InterruptedException{
+    public ResponseEntity sendMoney(@RequestBody PaymentRequest paymentRequest) throws InterruptedException{
 
         return new ResponseEntity<>(Map.of("message","Payment sent"), HttpStatus.OK);
     }
---
+++ src/main/java/com/payments/vipay/data/PaymentRequest.java
@@ -0,0 +1,4 @@
+package com.payments.vipay.data;
+
+public class PaymentRequest {
+}
