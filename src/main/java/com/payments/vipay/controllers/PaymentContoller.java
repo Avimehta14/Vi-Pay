f5780f661ec92caa354b69f6e7351e7e7bff3caa
avimehta14 <avimehta14@gmail.com>
2024-08-27 23:41:22 -0500

    kafka templates

--- src/main/java/com/payments/vipay/controllers/PaymentContoller.java
+++ src/main/java/com/payments/vipay/controllers/PaymentContoller.java
@@ -1,7 +1,9 @@
 package com.payments.vipay.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/pay")
 public class PaymentContoller {
+
+    @Autowired
+    private final KafkaTemplate<String,PaymentRequest> kafkaTemplate;
+
 
     @PostMapping
     public ResponseEntity sendMoney() throws InterruptedException{
