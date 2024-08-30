1e2c2755f20343ef9676427901c09e48e42e745c
avimehta14 <avimehta14@gmail.com>
2024-08-29 23:33:23 -0500

    error handler logger

--- src/main/java/com/payments/vipay/config/KafkaConfig.java
+++ src/main/java/com/payments/vipay/config/KafkaConfig.java
@@ -1,4 +1,67 @@
 package com.payments.vipay.config;
 
+import com.payments.vipay.data.PaymentRequest;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
 public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, PaymentRequest> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentRequest> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, PaymentRequest> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        // Retry 3 times with a 1-second interval between retries
+        FixedBackOff fixedBackOff = new FixedBackOff(1000L, 3);
+        return new DefaultErrorHandler(fixedBackOff);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentRequest> kafkaListenerContainerFactory(
+            ConsumerFactory<String, PaymentRequest> consumerFactory,
+            DefaultErrorHandler errorHandler) {
+        ConcurrentKafkaListenerContainerFactory<String, PaymentRequest> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
+        return factory;
+    }
 }
--- src/main/java/com/payments/vipay/events/PaymentServiceConsumer.java
+++ src/main/java/com/payments/vipay/events/PaymentServiceConsumer.java
@@ -3,9 +3,12 @@ package com.payments.vipay.events;
 import com.payments.vipay.data.PaymentRequest;
 import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PaymentServiceConsumer
 {
+    private static final Logger logger = LoggerFactory.getLogger(PaymentServiceConsumer.class);
 
     @KafkaListener(topics= "payment_initiated", groupId = "payment-group", containerFactory = "kafkaListenerContainerFactory")
     public void topicCosnumer(PaymentRequest paymentRequest) throws Exception {
@@ -22,14 +25,17 @@ public class PaymentServiceConsumer
 
         if (paymentRequest.getAmount() < 0)
         {
+            logger.error("Payament is invalid");
             throw new Exception("Inavlid Payment Amount");
         }
         System.out.println("payment Processed Successfuly");
+        logger.error("payment not processed !!!!!");
     }
 
     @DltHandler
     public void handleDlt(PaymentRequest paymentRequest)
     {
+        logger.error("DLT handling the error");
         System.err.println("handling error in message" + paymentRequest);
     }
 }
